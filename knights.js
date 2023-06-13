3ca8f8ea69cccb5543d96add0bd24f1177d94ccb
dimzles <dimzlestv@gmail.com>
2023-06-13 15:22:39 +0100

    Create addVertices function

--- knights.js
+++ knights.js
@@ -2,6 +2,14 @@ class Graph {
     constructor() {
         this.chessBoard = new Map();
     }
+    
+    addVertices(size = 8) { //Size of chessboard is 8x8 as standard
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                this.chessBoard.set(`${[i, j]}`, [])
+            }
+        }
+    }
 }
 
 
