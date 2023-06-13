7121f873fcf66ed61bab2795d33c384e6a20bde5
dimzles <dimzlestv@gmail.com>
2023-06-13 15:27:52 +0100

    Create addEdges function

--- knights.js
+++ knights.js
@@ -10,6 +10,31 @@ class Graph {
             }
         }
     }
+
+    
+    addEdges(board = this.chessBoard) {
+        for (let [pos] of board) {
+            const positionArray = pos.split(',');
+            const x = parseInt(positionArray[0]);
+            const y = parseInt(positionArray[1]);
+
+            const knightOffsets = {
+                1: [ x + 1, y + 2 ],
+                2: [ x + 2, y + 1 ],
+                4: [ x + 2, y - 1 ],
+                5: [ x + 1, y - 2 ],
+                7: [ x - 1, y - 2 ],
+                8: [ x - 2, y - 1 ],
+                10: [ x - 2, y + 1 ],
+                11: [ x - 1, y + 2 ]
+            }
+            
+            for (let direction in knightOffsets) {
+                const move = knightOffsets[direction].toString();
+                if (board.has(move) && !board.get(pos).includes(move)) this.chessBoard.get(pos).push(move) 
+            }
+        }
+    }
 }
 
 
